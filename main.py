cb37e46e5371d953f37b50d6e6810e3936b226a6
Javierdiaz6g <javierdiaz6@gmail.com>
2026-08-05 17:17:10 -0500

    Completed menu assignment

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-def main():
-  print("Hello learners!")
+import requests
+def dish_fetch(num):
+    response = requests.get(
+        "https://api-colombia.com/api/v1/TypicalDish"
+        )
+    platos = response.json()
+    if num < 0 or num >= len(platos):
+          return {}
+    return platos[num]
 
-if __name__=="__main__":
-  main()
+def main():
+     response = requests.get(
+          "https://api-colombia.com/api/v1/TypicalDish"
+          )
+     platos = response.json()
+
+     print("=== MENÚ DE PLATOS TÍPICOS DE COLOMBIA ===")
+
+     for i, plato in enumerate(platos):
+          print(f"{i}. {plato['name']}")
+          opcion = int(input("\nSeleccione un plato: "))
+          plato = dish_fetch(opcion)
+          if plato:
+              print("\nNombre:", plato["name"])
+              print("Descripción:", plato["description"])
+
+if __name__ == "__main__":
+     main()
