5855ce1d6b3de13df0570410fb8c3b4873abc673
Javierdiaz6g <javierdiaz6@gmail.com>
2026-08-05 19:12:12 -0500

    Lo subo tarde pero funciona y yo lo realice con mi logica

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
+import json
 import requests
+
+
 def dish_fetch(num):
     response = requests.get(
         "https://api-colombia.com/api/v1/TypicalDish"
         )
     platos = response.json()
-    if num < 0 or num >= len(platos):
-          return {}
-    return platos[num]
+    if num < 0 or num >= len(platos):      
+          return{}
+    plato = platos[num - 1]
+    return {"id": plato['id'], "name": plato['name'], "description": plato['description']}
+
 
 def main():
-     response = requests.get(
-          "https://api-colombia.com/api/v1/TypicalDish"
-          )
-     platos = response.json()
+    while True:
+        print("\n_________________________________________________________________________")
+        print("\n|*< Bienvenidos al programa de Búsqueda de Platos Típicos de Colombia >*|")
 
-     print("=== MENÚ DE PLATOS TÍPICOS DE COLOMBIA ===")
-
-     for i, plato in enumerate(platos):
-          print(f"{i}. {plato['name']}")
-          opcion = int(input("\nSeleccione un plato: "))
-          plato = dish_fetch(opcion)
-          if plato:
-              print("\nNombre:", plato["name"])
-              print("Descripción:", plato["description"])
-
-if __name__ == "__main__":
-     main()
+        codigo = input("\n|*< Ingrese el código del plato (o escriba 'salir'):                > \n ")
+        if codigo.lower() == "salir":
+            print("👋 Gracias por usar el programa.")
+            break
+        elif not codigo.isdigit():
+            print("❌ Por favor, ingrese un número válido.")
+            continue
+        num = int(codigo)
+        print(f"|*< Buscando el plato con código {codigo}...                                 >*|\n")
+        resultado = dish_fetch(num)
+        print(f"|*< ID: {resultado['id']}                                                     ")
+        print(f"|*< Nombre: {resultado['name']}                                               ")
+        print(f"|*< Descripcion: {resultado['description']}                                     ")
+if __name__=="__main__":
+    main()
